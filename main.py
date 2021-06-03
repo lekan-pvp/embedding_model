3a73f35b76b9e20e015080e644f8a94a43f0b201
Vladislav Popov <lekan.lekan@gmail.com>
2021-06-03 20:52:20 +0300

    embedding

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import tensorflow as tf
 
+def get_target_and_size(sequence, target_index, window_size):
+    target_word = sequence[target_index]
+    half_window_size = window_size // 2
+    return (target_word, half_window_size)
+
+def get_window_indices(sequence, target_index, half_window_size):
+    left_incl = max(0, target_index - half_window_size)
+    right_excl = min(len(sequence), target_index + half_window_size + 1)
+    return left_incl, right_excl
+
 # Skip-gram embedding model
 class EmbeddingModel(object):
     # Model initialization
